bcc270aebdade6c67a434e2101cad3e81d0691df
eddy.rodriguez <e.rodriguezd@pucp.edu.pe>
2022-05-02 15:10:15 -0500

    Added React-Bootstrap to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import ProductCard from './components/ProductCard/ProductCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <ProductCard/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
